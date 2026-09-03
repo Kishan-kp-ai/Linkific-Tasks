4f3b569941d7f30f6d39b5e91d8098935beaee2c
Kishan-kp-ai <kishanpauskar11@gmail.com>
2026-09-03 18:24:07 +0530

    Update practice.py

--- Day 2/practice.py
+++ Day 2/practice.py
@@ -8,3 +8,37 @@ elif marks < 75:
     print("Good")
 else:
     print("Excellent")
+
+# Variables
+name = "Kishan"
+age = 21
+marks = 85.5
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+ # Conditional statement
+if marks >= 40:
+    print("Student has passed")
+else:
+    print("Student has failed")
+
+ # For loop
+numbers = [10, 20, 30, 40, 50]
+
+for number in numbers:
+    print(number)
+
+# While loop
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+# Function
+def greet(name):
+    return "Hello " + name
+
+print(greet("Kishan"))
