cef7cace441816022b1abf44063d7f93c7d0bdf8
Kishan-kp-ai <kishanpauskar11@gmail.com>
2026-09-03 18:16:34 +0530

    Create practice.py

---
+++ Day 2/practice.py
@@ -0,0 +1,10 @@
+marks = float(input("Enter your marks: "))
+
+if marks < 40:
+    print("Fail")
+elif marks < 60:
+    print("Pass")
+elif marks < 75:
+    print("Good")
+else:
+    print("Excellent")
